e804a1821b3aafa37b0b51faf773eb68290715a0
guerent <troy.guerena@ihg.com>
2016-12-28 12:18:23 -0500

    Initial commit for MQTT Retains Viewer

---
+++ showMQTTRetains.php
@@ -0,0 +1,39 @@
+<?php
+ini_set('display_errors', 'On');
+ini_set('html_errors', 0);
+
+$shortopts  = "";
+$shortopts .= "i:";  // IP Address Required value
+$shortopts .= "p::"; // Port Number Optional value
+$shortopts .= "h::"; // Help
+
+$longopts  = array(
+    "help::",
+    "ip::",
+    "port::",        // No value
+);
+
+$options = getopt($shortopts, $longopts);
+if ((isset($options['help']) || isset($options['h'])) || ((filter_var($options['i'], FILTER_VALIDATE_IP) === false) && (filter_var($options['ip'], FILTER_VALIDATE_IP) === false))){
+    echo "\nThis tool is used to check the retains stored in MQTT Server. Use clearRetains to clear all retains to start fresh.
+    -i IP address of MQTT Server (can also use --ip) [!REQUIRED]
+    -p Port number of MQTT Server (can also use --port) [No entry will default to port 8883]
+    ";
+} else {
+    $ip = filter_var($options['i'], FILTER_VALIDATE_IP) === false ? $options['i'] : $options['ip'];
+    $cmd = "mosquitto_sub -h $ip -t 'smartthings/#' -v";
+    $cmd .= !empty($options['p']) ? "-p ".$options['p'] : !empty($options['port']) ? "-p ".$options['port'] : "";
+    $descriptorspec = array(
+        0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
+        1 => array("pipe", "w"),   // stdout is a pipe that the child will write to
+        2 => array("pipe", "w")    // stderr is a pipe that the child will write to
+    );
+    flush();
+    $process = proc_open($cmd, $descriptorspec, $pipes, realpath('./'), array());
+    while ($s = fgets($pipes[1])) {
+        echo $s;
+    }
+}
+
+
+print_r($options);
